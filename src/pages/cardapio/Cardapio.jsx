004650ddbc6b8a946fd7a64084ffb03d847f3993
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-06-20 16:20:31 -0300

    ajuste

--- src/pages/cardapio/Cardapio.jsx
+++ src/pages/cardapio/Cardapio.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './cardapio.css'
 import Header from '../../components/header/Header.jsx'
 import { Card } from '../../components/card/card.jsx'
@@ -8,12 +8,31 @@ import { ShopCar } from '../../context/ContextValueCar'
 
 
 export default function Cardapio() {
+
+  const [data, setData] = useState({});
   
   const {items, setItems, valuesItems, setValuesItems} = useContext(ShopCar);
 
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch('http://localhost:3030/produtos');
+        if (!response.ok) {
+          throw new Error('Falha ao carregar os dados');
+        }
+        const data = await response.json();
+        setData(data); // Define os produtos no estado
+        console.log(data)
+      } catch (error) {
+        console.error('Erro ao carregar os produtos:', error);
+      }
+    };
+
+    fetchProdutos();
+  }, []);
 
   return (<>
-  
+       
     <Header item={0} valueCount={0}/>
 
     <section className="cardSection">
