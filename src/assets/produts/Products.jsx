b152bb1e1c9d45f88d3312c002047b9f154b6c16
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2025-03-25 17:02:05 -0300

    AJUSTANDO

---
+++ src/assets/produts/Products.jsx
@@ -0,0 +1,44 @@
+export const products = [
+    {
+        name: 'Açaí',
+        litragem: '1000',
+        description: 'Açaí batido com Morango',
+        flavor: 'Morango',
+        price: 26.99,
+    },
+    {
+        name: 'Açaí ',
+        litragem: '1000',
+        description: 'Açaí batido com Banana',
+        flavor: 'Banana',
+        price: 26.99,
+    },
+    {
+        name: 'Açaí ',
+        litragem: '1000',
+        description: 'Açaí natural guarana da Amazônia',
+        flavor: 'Natural',
+        price: 26.99,
+    },
+    {
+        name: 'Açaí',
+        litragem: '500',
+        description: 'Açaí batido com Morango',
+        flavor: 'Morango',
+        price: 15.99,
+    },
+    {
+        name: 'Açaí',
+        litragem: '500',
+        description: 'Açaí batido com Banana',
+        flavor: 'Banana',
+        price: 15.99,
+    },
+    {
+        name: 'Açaí',
+        litragem: '500',
+        description: 'Açaí natural guarana da Amazônia',
+        flavor: 'Natural',
+        price: 15.99,
+    },
+]
